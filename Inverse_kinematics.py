20a1dd3eae57b1df9a773865d2b461cb55a19c91
Coleslaw1 <61027282+Coleslaw1@users.noreply.github.com>
2020-06-17 10:42:02 +0200

    Added steps to inv_kin.py

--- Inverse_kinematics.py
+++ Inverse_kinematics.py
@@ -4,9 +4,8 @@ from math import sin, cos, acos, atan, degrees, pow
 import matplotlib.pyplot as plt
 
 ##To do:
-# - Steps meenemen in berekening, dit een float maken en vervolgens afronden
 
-DEBUG = 0
+DEBUG = 1
 
 la1 = 215 #Millimeters
 la2 = 165 #Millimeters
@@ -36,6 +35,7 @@ q2_difference_array = []
 x1_pos_array=[]
 y1_pos_array=[]
 
+
 def angle_steps(q2,q1):
     stepsPerDegree = stepsRotation60T/360
     steps_q2 = stepsPerDegree*q2
@@ -53,13 +53,16 @@ def rounded_degrees(q2,q1):
 def inv_kinematics(x,y, q2_deg_old, q1_deg_old):
     q2 = acos((pow(x,2)+pow(y,2)-pow(la1,2)-pow(la2,2))/(2*la1*la2))
     q1 = atan(y/x)-atan((la2*sin(q2))/(la1+la2*cos(q2)))
-    q2,q1 = angle_steps(q2,q1) #Hier wordt al afgerond en dat is niet wenselijk
+    q2,q1 = angle_steps(q2,q1) 
+    
     opening_angle = q2-q1
+    if(DEBUG):
+        print("Opening angle: ", opening_angle)
     if(opening_angle > arm_2_angle_limit or opening_angle < -arm_2_angle_limit):
         try:
             q2 = -acos((pow(x,2)+pow(y,2)-pow(la1,2)-pow(la2,2))/(2*la1*la2))
             q1 = atan(y/x)+atan((la2*sin(q2))/(la1+la2*cos(q2))) 
-            q2,q1 = angle_steps(q2,q1) #Hier wordt ook al afgerond
+            q2,q1 = angle_steps(q2,q1)
         except:
             print("No possible angle combination found")
     
